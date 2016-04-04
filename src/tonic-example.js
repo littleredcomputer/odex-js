2749bb146d4c0a4d4585498ad523e4cd182338b0
Colin Smith <colin.smith@gmail.com>
2016-04-03 19:13:28 -0700

    fix tonic example

--- src/tonic-example.js
+++ src/tonic-example.js
@@ -1,6 +1,9 @@
 var odex = require("odex");
 var s = new odex.Solver(2);
-var eq = function(x, y, yprime) {
+var eq = function(x, y) {
     return [y[1], -y[0]];
 };
+// This is y'' = -y, y(0) = 1, y'(0) = 0, e.g. cos(x)
 s.solve(eq, 0, [1, 0], Math.PI);
+// We observe that at x = π, y is near -1 and y' is near 0,
+// as expected.
