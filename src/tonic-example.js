c4aa8ac6b0c9bfa0f1c3eafd2338e5116db12293
Colin Smith <colin.smith@gmail.com>
2023-02-05 15:26:50 -0800

    update tonic-example to new API

--- src/tonic-example.js
+++ src/tonic-example.js
@@ -1,9 +1,10 @@
 var odex = require("odex");
-var s = new odex.Solver(2);
 var eq = function(x, y) {
     return [y[1], -y[0]];
 };
+var s = new odex.Solver(eq, 2);
 // This is y'' = -y, y(0) = 1, y'(0) = 0, e.g. cos(x)
-s.solve(eq, 0, [1, 0], Math.PI);
+f = s.integrate(0, [1, 0])
+f(Math.PI)
 // We observe that at x = π, y is near -1 and y' is near 0,
 // as expected.
